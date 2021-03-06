1d200751c81526f43e61729803d8e3a8a17493c7
Hortlak46 <Administrator@SINARailesi.home>
2021-03-06 13:21:33 +0300

    second commit

---
+++ src/main/java/net/mcreator/proje/item/BsdfgbItem.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.proje.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.proje.ProjeModElements;
+
+@ProjeModElements.ModElement.Tag
+public class BsdfgbItem extends ProjeModElements.ModElement {
+	@ObjectHolder("proje:bsdfgb")
+	public static final Item block = null;
+	public BsdfgbItem(ProjeModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 100;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("bsdfgb"));
+	}
+}
